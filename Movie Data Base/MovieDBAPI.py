d25f431ba6387f09583a33b4ca463123070eb296
MangroveCuckoo <mueller.cara@gmail.com>
2017-02-09 19:57:50 -0800

    Add a Readme and better commenting

--- Movie Data Base/MovieDBAPI.py
+++ Movie Data Base/MovieDBAPI.py
@@ -7,6 +7,7 @@ from apikey import api_key
 lang = {'language': 'en-US'}
 base_url = 'https://api.themoviedb.org/3/movie/'
 
+#gets the movie associated with the movie ID and returns a json object
 def callmovieapi(base_url, movie_number):
 	url = base_url + str(movie_number)
 	r = requests.get(url, params=api_key)
@@ -14,6 +15,7 @@ def callmovieapi(base_url, movie_number):
 		raw_movie = r.json()
 		return raw_movie
 	
+#returns a json object of the latest movie ID in the MovieDB database
 def latestmovie(base_url):
 	url = base_url + 'latest'
 	try:
@@ -24,6 +26,7 @@ def latestmovie(base_url):
 	raw_movie = r.json()
 	return raw_movie
 	
+#prints the header info to the csv file
 def printcsvheader(csv_file, column_names):
 	try:
 		with open(csv_file, 'wb') as csvfile:
@@ -33,6 +36,7 @@ def printcsvheader(csv_file, column_names):
 		print("I/O Error({0}): {1}".format(errno, strerror))
 	return
 	
+#prints the list of formated data to the csv file
 def printtocsv(csv_file, column_names, movie):
 	try:
 		with open(csv_file, 'ab') as csvfile:
@@ -44,13 +48,15 @@ def printtocsv(csv_file, column_names, movie):
 	return
 			
 
+#strips all relevant info from the json object, and creates a list
+#also formats in utf-8 since foreign characters are used in MovieDB
 def formatmoviedict(raw_movie):
 	movie = [{'id': raw_movie['id'],
 	          'original_title': raw_movie['original_title'].encode('utf-8'),
 	          'overview': raw_movie['overview'].encode('utf-8')}]
 	return movie
 
-
+#validates the data coming from MovieDB
 def validate(raw_movie):
     #All the things we think are true about a raw_movie dict
 	if raw_movie is None:
@@ -64,19 +70,21 @@ def validate(raw_movie):
 currentPath = os.getcwd()
 csv_file = currentPath + "/csv/movies.csv"
 
+#column names used in the csv file
 column_names = ['id','original_title','overview'] 
 
 
 # Exports the header info to a csv file
 printcsvheader(csv_file, column_names)
 
-
+#this is used to avoid hitting the MovieDB API too often
 delay_counter = 0
+
+#finds the last entry in MovieDB
 total_movies = latestmovie(base_url)
 
-#this loop will eventually end in the id number of total_movies
-#for movie_number in range(1,total_movies['id']):
-for movie_number in range(550,550):
+#loops through all movies IDs in MovieDB and extracts the wanted data
+for movie_number in range(1,total_movies['id']):
 
 	if delay_counter == delay_counter + 40:
 		time.sleep(10)
---
+++ Movie Data Base/README.md
@@ -0,0 +1,8 @@
+This program will hit every entry in the MovieDB database and pull the ID number,
+Title, and Plot Summary from every valid entry. It then outputs the data into a
+csv file.
+
+To use this you will need an API key from MovieDB. You will also need to create
+a csv file to drop all the data into. If you plan on pulling anything other than
+the ID number, the Title, and the Plot Summary from MovieDB, you will need to 
+modify the code in several places. 
